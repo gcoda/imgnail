f0277db13e286a0efa477333901dc03cdf694729
gcoda <gcoding@gmail.com>
2018-12-22 22:24:50 +0200

    fix

--- examples/send.example.js
+++ examples/send.example.js
@@ -1,15 +1,15 @@
 require('dotenv').config()
-
 const { stringify, parse } = require('json-cipher-url')(process.env.NAIL_SECRET)
 
 const source = {
+  purge: true, // only relevant when making request to purge function
   url: 'https://storage.googleapis.com/anigen/screenshot/1/260/firstFrame.jpg',
   resize: {
-    width: '300',
-    height: '300',
+    width: 100,
+    height: 100,
     fit: 'cover',
   },
-  output: { format: 'jpeg', quality: 9 },
+  output: { format: 'jpeg', quality: 70 },
 }
 
 const prefix = 'https://d1fofjl7fd4aao.cloudfront.net/'
