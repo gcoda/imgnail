df8f0397d8bffff945ce84b795bec8fcb9b7a59b
gcoda <gcoding@gmail.com>
2018-12-22 22:23:21 +0200

    export purge

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const sign = require('./functions/sign')
 const send = require('./functions/send')
+const purge = require('./functions/purge')
 
 exports.sign = sign
 exports.send = send
+exports.purge = purge
